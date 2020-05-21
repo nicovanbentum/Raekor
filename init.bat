a91fa6208fcff5bb38d080706e4f2d22ccbcdbd0
Nico van Bentum <31741718+nicovanbentum@users.noreply.github.com>
2020-05-21 06:33:34 +0200

    added batch file for building deps

---
+++ init.bat
@@ -0,0 +1,14 @@
+@echo off
+
+vcpkg install assimp --triplet x64-windows
+vcpkg install bullet3 --triplet x64-windows
+vcpkg install sdl2 --triplet x64-windows
+xcopy /s "installed\x64-windows\include\SDL2" "installed\x64-windows\include" /Y > nul
+vcpkg remove sdl --triplet x64-windows
+vcpkg remove sdl1 --triplet x64-windows
+vcpkg install directxtk --triplet x64-windows
+
+echo.
+echo Initialization finished.
+echo.
+pause
