001a4382faf928c66b68cef3189e7da7f15a60d3
Nico van Bentum <niico0708@gmail.com>
2020-08-31 16:42:37 +0200

    vcpkg sdl2  vulkan support
    Apparently vcpkg's cmake installation of SDL2 has Vulkan turned off by default.

--- init.bat
+++ init.bat
@@ -2,7 +2,7 @@
 
 vcpkg install assimp --triplet x64-windows
 vcpkg install bullet3 --triplet x64-windows
-vcpkg install sdl2 --triplet x64-windows
+vcpkg install sdl2[vulkan] --triplet x64-windows
 xcopy /s "installed\x64-windows\include\SDL2" "installed\x64-windows\include" /Y > nul
 vcpkg remove sdl --triplet x64-windows
 vcpkg remove sdl1 --triplet x64-windows
