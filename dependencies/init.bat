62d06ffe1d246ebcb9ad0e2feffc21a568dbc424
Nico van Bentum <niico0708@gmail.com>
2021-01-12 23:45:18 +0100

    init bat update

--- dependencies/init.bat
+++ dependencies/init.bat
@@ -2,13 +2,13 @@
 
 python -m glad/glad --generator=c --out-path=GL --profile=core --api=gl=4.6
 
-vcpkg/bootstrap-vcpkg.bat
+call "vcpkg/bootstrap-vcpkg.bat"
 
-vcpkg/vcpkg install lz4:x64-windows-static
-vcpkg/vcpkg install assimp:x64-windows-static
-vcpkg/vcpkg install physx:x64-windows-static
-vcpkg/vcpkg install sdl2[vulkan]:x64-windows-static
-vcpkg/vcpkg install spirv-cross:x64-windows-static
+"vcpkg/vcpkg.exe" install lz4:x64-windows-static
+"vcpkg/vcpkg.exe" install assimp:x64-windows-static
+"vcpkg/vcpkg.exe" install physx:x64-windows-static
+"vcpkg/vcpkg.exe" install sdl2[vulkan]:x64-windows-static
+"vcpkg/vcpkg.exe" install spirv-cross:x64-windows-static
 
-vcpkg/vcpkg integrate project
+"vcpkg/vcpkg.exe" integrate project
 
