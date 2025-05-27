a252e0d724a80865e6bfe32cef455aa3aee143ff
Nico van Bentum <niico0708@gmail.com>
2025-05-28 01:20:47 +0200

    Format

--- Code/Engine/Undo.cpp
+++ Code/Engine/Undo.cpp
@@ -5,32 +5,32 @@ namespace RK {
 
 void UndoSystem::Undo()
 {
-	if (!HasUndo())
-		return;
+    if (!HasUndo())
+        return;
 
-	//std::cout << "Undo " << m_UndoStack.back()->GetName() << " at index " << m_UndoStack.size() - 1 << std::endl;
+    //std::cout << "Undo " << m_UndoStack.back()->GetName() << " at index " << m_UndoStack.size() - 1 << std::endl;
 
-	m_UndoStack.back()->Undo(m_Scene);
-	m_RedoStack.push_back(m_UndoStack.back());
-	m_UndoStack.pop_back();
+    m_UndoStack.back()->Undo(m_Scene);
+    m_RedoStack.push_back(m_UndoStack.back());
+    m_UndoStack.pop_back();
 }
 
 void UndoSystem::Redo()
 {
-	if (!HasRedo())
-		return;
+    if (!HasRedo())
+        return;
 
-	//std::cout << "Redo " << m_RedoStack.back()->GetName() << " at index " << m_RedoStack.size() - 1 << std::endl;
+    //std::cout << "Redo " << m_RedoStack.back()->GetName() << " at index " << m_RedoStack.size() - 1 << std::endl;
 
-	m_RedoStack.back()->Redo(m_Scene);
-	m_UndoStack.push_back(m_RedoStack.back());
-	m_RedoStack.pop_back();
+    m_RedoStack.back()->Redo(m_Scene);
+    m_UndoStack.push_back(m_RedoStack.back());
+    m_RedoStack.pop_back();
 }
 
 void UndoSystem::Clear()
 {
-	m_UndoStack.clear();
-	m_RedoStack.clear();
+    m_UndoStack.clear();
+    m_RedoStack.clear();
 }
 
 }
--- Code/Engine/Undo.h
+++ Code/Engine/Undo.h
@@ -9,59 +9,59 @@ namespace RK {
 
 struct UndoAction
 {
-	virtual ~UndoAction() = default;
+    virtual ~UndoAction() = default;
 
-	virtual const char* GetName() = 0;
-	virtual void Undo(Scene& inScene) = 0;
-	virtual void Redo(Scene& inScene) = 0;
+    virtual const char* GetName() = 0;
+    virtual void Undo(Scene& inScene) = 0;
+    virtual void Redo(Scene& inScene) = 0;
 };
 
 class UndoSystem
 {
 public:
-	UndoSystem(Scene& inScene) : m_Scene(inScene) { }
+    UndoSystem(Scene& inScene) : m_Scene(inScene) {}
 
-	template<typename T> requires std::is_base_of_v<UndoAction, T>
-	void PushUndo(const T& inAction) 
-	{ 
-		m_RedoStack.clear();
-		m_UndoStack.push_back(std::make_shared<T>(inAction)); 
-		//std::cout << "Pushed " << m_UndoStack.back()->GetName() << " at index " << m_UndoStack.size() - 1 << std::endl;
-	}
+    template<typename T> requires std::is_base_of_v<UndoAction, T>
+    void PushUndo(const T& inAction)
+    {
+        m_RedoStack.clear();
+        m_UndoStack.push_back(std::make_shared<T>(inAction));
+        //std::cout << "Pushed " << m_UndoStack.back()->GetName() << " at index " << m_UndoStack.size() - 1 << std::endl;
+    }
 
-	bool HasUndo() const { return !m_UndoStack.empty(); }
-	bool HasRedo() const { return !m_RedoStack.empty(); }
+    bool HasUndo() const { return !m_UndoStack.empty(); }
+    bool HasRedo() const { return !m_RedoStack.empty(); }
 
-	void Undo();
-	void Redo();
-	void Clear();
+    void Undo();
+    void Redo();
+    void Clear();
 
 private:
-	Scene& m_Scene;
-	Array<SharedPtr<UndoAction>> m_UndoStack;
-	Array<SharedPtr<UndoAction>> m_RedoStack;
+    Scene& m_Scene;
+    Array<SharedPtr<UndoAction>> m_UndoStack;
+    Array<SharedPtr<UndoAction>> m_RedoStack;
 };
 
-template<typename T> requires (sizeof(T) < 1024 && std::default_initializable<T> )
-struct ComponentUndo : public UndoAction
+template<typename T> requires ( sizeof(T) < 1024 && std::default_initializable<T> )
+    struct ComponentUndo : public UndoAction
 {
-	T current;
-	T previous;
-	Entity entity = Entity::Null;
+    T current;
+    T previous;
+    Entity entity = Entity::Null;
 
-	virtual const char* GetName() { return RTTI_OF<T>().GetTypeName(); }
+    virtual const char* GetName() { return RTTI_OF<T>().GetTypeName(); }
 
-	void Undo(Scene& inScene) override
-	{
-		T& component = inScene.Get<T>(entity);
-		component = previous;
-	}
+    void Undo(Scene& inScene) override
+    {
+        T& component = inScene.Get<T>(entity);
+        component = previous;
+    }
 
-	void Redo(Scene& inScene) override
-	{
-		T& component = inScene.Get<T>(entity);
-		component = current;
-	}
+    void Redo(Scene& inScene) override
+    {
+        T& component = inScene.Get<T>(entity);
+        component = current;
+    }
 };
 
 }
